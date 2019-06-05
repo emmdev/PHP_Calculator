6c52186d980a37145a1687f1029879760749abff
Emmanuel <emmanuel.devries@trios.com>
2019-06-05 15:37:04 -0400

    cleaned up the view

--- calculator.php
+++ calculator.php
@@ -15,24 +15,9 @@ include("calculator_controller.php");
 	</style>
 </head>
 <body>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-        <input type="text" name="number1" value="<?= $number1 ?>">
-        
-        <input type="submit" name="add" value="+">
-        <input type="submit" name="sub" value="-">
-        <input type="submit" name="mul" value="&times;">
-        <input type="submit" name="div" value="&divide;">
-        
-        <input type="text" name="number2" value="<?= $number2 ?>"><br>
-        
-    </form>
-    
-    <p><?= $answer ?></p>
-    
-    <h2>History of Calculations</h2>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-        <input type="submit" name="clear_history" value="clear">
-    </form>
+
+    <h2>Messages</h2>
+
     <ul>
         <?php
         foreach($history_of_calculations_array as $line) {
@@ -40,6 +25,18 @@ include("calculator_controller.php");
         }
         ?>
     </ul>
+    
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+        <input type="text" name="number1" value="<?= $number1 ?>">
+        
+        <input type="submit" name="add" value="+">
+        
+    </form>
+    
+    <br />
 
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+        <input type="submit" name="clear_history" value="clear">
+    </form>
 </body>
 </html>
