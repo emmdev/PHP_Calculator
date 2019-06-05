9b22ebd19b8dabae49cb6d23061954babf5b8aa1
Emmanuel <emmanuel.devries@trios.com>
2019-06-05 15:35:32 -0400

    cleaned up the controller

--- calculator_controller.php
+++ calculator_controller.php
@@ -19,20 +19,13 @@ if( isset($_POST['clear_history']) ) {
 $form_has_been_submitted = isset($_POST['number1']);
 
 // defaults, in case the form has not been submitted
-$number1 = 0;
-$number2 = 0;
+$number1 = "";
 $answer = 0;
 $calculation_text = "";
 
 // only process form variables if the form was submitted... avoids errors
 if( $form_has_been_submitted ) {
     $number1 = $_POST['number1'];
-    $number2 = $_POST['number2'];
-    
-    // handle non-numeric inputs from our form, avoids errors
-    //$number1 = (float)$number1;
-    $number2 = (float)$number2;
-    
     
     $answer = $number1;
     $calculation_text = $number1;
